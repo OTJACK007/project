bcbd30d3e25637b93909fac6d009a8755f9a523e
Ismael <ismailasockpro@gmail.com>
2024-12-01 19:50:47 +0100

    Revert "probleme de deploiement"
    This reverts commit ec3cfdd79946768cfc542a86e680d77c18de705d.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export',  // Enable static exports
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -10,12 +10,6 @@ const nextConfig = {
       }
     ],
   },
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true
-  }
 };
 
 module.exports = nextConfig;
